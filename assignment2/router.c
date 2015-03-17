59238b73ddeb2d98683fe93abbfbbbb689b66904
derekdowling <me@derekdowling.com>
2015-03-16 23:06:57 -0600

    Working generator and router

--- assignment2/router.c
+++ assignment2/router.c
@@ -1,25 +1,145 @@
 /**
- * A very simple packet router. 
- *
+ * A very simple packet router.
  */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <strings.h>
-#include "router.h"
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
+#include <limits.h>
 
-void launch (int argc, char *argv[]) {
+#define MAX_BUFFER 65535
+#define IP 2130706433 /* 127.0.0.1 */
+
+/* Struct Definitions */
+typedef struct {
+    char* address;
+    int prefix_length;
+    char* next_hop;
+} Router;
+
+typedef struct {
+    Router** routes;
+    int size;
+    int max_size;
+} RouterTable;
+
+typedef struct {
+    int id;
+    char* src;
+    char* dest;
+    int TTL;
+    char* payload;
+} Packet;
+
+typedef struct {
+    int expired;
+    int unroutable;
+    int direct;
+    int router_b;
+    int router_c;
+} Stats;
+
+
+/* Function Definitions */
+RouterTable* RouterTable_new();
+RouterTable* build_router_table(char* table_path);
+void add_new_router(RouterTable* table, char address[16], int prefix_length, char next_hop[8]);
+int build_packet(Packet* packet, char* raw_packet);
+Packet Packet_new(int id, char* src, char* dest, int TTL, char* payload);
+void route_packet(FILE* stats_file, RouterTable* table, char* stream);
+int find_destination_router(Router* router, RouterTable* table, Packet* packet);
+void output_statistics(FILE* stats_file);
+int build_socket(int port);
+int set_server_address(RouterTable* table);
+void signal_handler(int signal);
+int compare_subnet(int prefix_length, char* candidate, char* destination);
+uint32_t parse_ipv4_string(char* ipAddress);
+
+/* Global Stats Struct */
+Stats stats;
+int keep_running = 1;
+
+int main(int argc, char *argv[])
+{
+	char raw_packet[MAX_BUFFER];
+	FILE* stats_file;
+	struct sockaddr_in inc_socket;
+	int socketfd, counter, port, sock_length = sizeof(inc_socket);
 
 	if (argc != 4)
 	{
 		printf("Bad Args, should be <listening-port> <routing-table-path> <statistics-file-path>");
+		exit(-1);
 	}
 
-	RouteTable* table;
+	port = atoi(argv[1]);
+	char* table_file_path = argv[2];
+	char* stats_file_path = argv[3];
 
 	// parse out routes into Route array pointer from RT_A.txt
-	table = build_route_table(argv[2]);
+	RouterTable* table = build_router_table(table_file_path);
+
+	// initialize the statistics struct
+	stats = (Stats) {0, 0, 0, 0, 0};
+
+	stats_file = fopen(stats_file_path, "w");
+	if (stats_file == NULL)
+	{
+		fprintf(stderr, "Error opening stats file.\n");
+		exit(-1);
+	}
+
+	socketfd = build_socket(port);
+
+    signal(SIGINT, signal_handler);
+
+	// listen infinitely for incoming packets
+	counter = 0;
+	while (keep_running)
+	{
+	    bzero(raw_packet, MAX_BUFFER);
+
+		// Waits until we receive something
+		if (recvfrom(
+				socketfd,
+				raw_packet,
+				MAX_BUFFER,
+				0,
+				(struct sockaddr*) &inc_socket,
+				&sock_length
+			) != -1
+		) {
+			printf("Packet: %s\n", raw_packet);
+
+		// route and increment counter
+			route_packet(stats_file, table, raw_packet);
+			counter++;
+			printf("%d\n", counter);
+		
+			// see if it's time to output statistcs
+			if (counter == 20)
+			{
+				counter = 0;
+				output_statistics(stats_file);
+			}
+		}
+		else
+		{
+			fprintf(stderr, "Recvfrom err#: %d\n", errno);
+		}
+	}
+
+	// now tear everything back down
+	close(socketfd);
+	fclose(stats_file);
 
 	for(int i = 0; i < table->size; i++)
 	{
@@ -29,13 +149,93 @@ void launch (int argc, char *argv[]) {
 	free(table);
 }
 
+void route_packet(FILE* stats_file, RouterTable* table, char* stream)
+{
+	Packet* packet = malloc(sizeof(Packet));
+	Router* router;
+	char* next_hop;
+
+	if (build_packet(packet, stream) != 0)
+	{
+		stats.expired = stats.expired + 1;
+		puts("expired");
+		free(packet);
+		return;
+	}
+
+	if (find_destination_router(router, table, packet) == -1)
+	{
+		puts("unroutable");
+		stats.unroutable = stats.unroutable + 1;
+	}
+	else
+	{
+		if (strcmp(router->next_hop, "0") == 0)
+		{
+			stats.direct = stats.direct + 1;
+			printf(
+				"Delivering direct: packet ID=%d, dest=%s\n",
+				packet->id,
+				router->address
+			);
+		}
+		else
+		{
+			// increment counters and free packet allocations
+			next_hop = router->next_hop;
+			if (strcmp(next_hop, "RouterB") == 0)
+			{
+				puts("router B");
+				stats.router_b = stats.router_b + 1;
+			}
+			else if (strcmp(next_hop, "RouterC") == 0)
+			{
+				puts("router C");
+				stats.router_c = stats.router_c + 1;
+			}
+		}
+
+		// finally free the created packet
+		free(packet);
+	}
+}
+
+/**
+ * Attempts to find the destination router for the given packet from the provided
+ * table.
+ * Returns a Router pointe or NULL if no matches were found.
+ */
+int find_destination_router(Router* router, RouterTable* table, Packet* packet)
+{
+	int prefix_length;
+
+	for(int i = 0; i < table->size; i++)
+	{
+		Router* candidate = table->routes[i];
+
+		// see if router and destination are in the same subnet
+		if ((compare_subnet(
+			candidate->prefix_length,
+			candidate->address,
+			packet->dest) == 0
+			)  &&
+			strcmp(candidate->address, packet->dest) == 0
+		) {
+			*router = *candidate;
+			return 0;
+		}
+	}
+
+	return -1;
+}
+
 /**
  * Parses out a route table struct from the provided table file path.
  */
-RouteTable* build_route_table(char* table_path)
+RouterTable* build_router_table(char* table_path)
 {
 	char* token = NULL;
-	RouteTable* table = RouteTable_new();
+	RouterTable* table = RouterTable_new();
 	FILE* table_file = fopen(table_path, "r");
 
 	if (table_file == NULL)
@@ -55,8 +255,10 @@ RouteTable* build_route_table(char* table_path)
 		{
 			continue;
 		}
-		add_new_route(table, address, prefix_length, next_hop);
+		add_new_router(table, address, prefix_length, next_hop);
 	}
+
+	fclose(table_file);
 
 	return table;
 }
@@ -64,22 +266,23 @@ RouteTable* build_route_table(char* table_path)
 /**
  * Allocates and initializes a new RouteTable.
  */
-RouteTable* RouteTable_new()
+RouterTable* RouterTable_new()
 {
-	RouteTable* table = malloc(sizeof(RouteTable));
+	RouterTable* table = malloc(sizeof(RouterTable));
 	table->size = 0;
 	table->max_size = 20;
-	table->routes = malloc(table->max_size * sizeof(Route*));
+	table->routes = malloc(table->max_size * sizeof(Router*));
 	return table;
 }
-
-
 
 /**
  * Handles parsing, and preparing an incoming packet for:
  * <packet ID>, <source IP>, <destination IP>, <TTL>, <payload>
+ *
+ * If the packet can't be properly parsed or the TTL is too low, discards
+ * packet and returns NULL.
  */
-Packet* build_packet(char* raw_packet)
+int build_packet(Packet* packet, char* raw_packet)
 {
 	int id, TTL;
 	char src[16], dest[16];
@@ -118,37 +321,45 @@ Packet* build_packet(char* raw_packet)
 		token = strtok(NULL, selector);
 	}
 
-	Packet* packet = Packet_new(id, src, dest, TTL, payload);
+	// If we didn't parse the five expected packet segments
+	if (counter != 5)
+	{
+		fprintf(stderr, "Error parsing %s into packet, skipping.\n", raw_packet);
+		free(payload);
+		return -1;
+	}
+
+	*packet = Packet_new(id, src, dest, TTL, payload);
 	if (packet->TTL <= 0)
 	{
-		free(packet);
-		return NULL;
+		free(payload);
+		return -1;
 	}
 
 	free(payload);
 
-	return packet;
+	return 0;
 }
 
 /**
  * Constructor for creating a new packet. Will automatically decrement TTL.
  */
-Packet* Packet_new(int id, char* src, char* dest, int TTL, char* payload)
+Packet Packet_new(int id, char* src, char* dest, int TTL, char* payload)
 {
-	Packet* packet = malloc(sizeof(Packet));
+	Packet packet;
 
 	// allocate and initialize values
-	packet->id = id;
-	packet->src = malloc(strlen(src) + 1);
-	strcpy(packet->src, src);
-	packet->dest = malloc(strlen(dest) + 1);
-	strcpy(packet->dest, dest);
+	packet.id = id;
+	packet.src = malloc(strlen(src) + 1);
+	strcpy(packet.src, src);
+	packet.dest = malloc(strlen(dest) + 1);
+	strcpy(packet.dest, dest);
 
 	// automatically decrement TTL right off the bat
-	packet->TTL = TTL - 1;
-	packet->payload = malloc(strlen(payload) + 1);
+	packet.TTL = TTL - 1;
+	packet.payload = malloc(strlen(payload) + 1);
 
-	strcpy(packet->payload, payload);
+	strcpy(packet.payload, payload);
 	return packet;
 }
 
@@ -159,10 +370,10 @@ Packet* Packet_new(int id, char* src, char* dest, int TTL, char* payload)
  * 1 - <net‐prefix‐length>
  * 2 - <nexthop>
  */
-void add_new_route(RouteTable* table, char* address, int prefix_length, char* next_hop)
+void add_new_router(RouterTable* table, char* address, int prefix_length, char* next_hop)
 {
 	// marshal data into Route
-	Route* new_route = malloc(sizeof(Route));
+	Router* new_route = malloc(sizeof(Router));
 	new_route->address = malloc(strlen(address) + 1);
 	strcpy(new_route->address, address);
 	new_route->prefix_length = prefix_length;
@@ -173,10 +384,120 @@ void add_new_route(RouteTable* table, char* address, int prefix_length, char* ne
 	if (table->size == table->max_size)
 	{
 		table->max_size *= 2;
-		table->routes = realloc(table->routes, table->max_size * sizeof(Route*));
+		table->routes = realloc(table->routes, table->max_size * sizeof(Router*));
 	}
 
 	// add route to the array
 	table->routes[table->size] = new_route;
 	table->size = table->size + 1;
+}
+
+/**
+ * Updates the statistics file based upon the state of the global statistics
+ * struct.
+ */
+void output_statistics(FILE* stats_file)
+{
+	// rewind pointer to the start of the file
+	rewind(stats_file);
+	fprintf(
+		stats_file,
+		"expired packets: %d\nunroutable packets: %d\ndelivered direct: %d\nrouter B: %d\nrouter C: %d\n",
+		stats.expired, stats.unroutable, stats.direct, stats.router_b, stats.router_c
+	);
+
+	printf("Router stats updated.\n");
+}
+
+/**
+ * Handles building the socket, binding, and listening it to the specified
+ * port.
+ */
+int build_socket(int port)
+{
+	struct sockaddr_in sock;
+	int socketfd;
+	
+	if ((socketfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+	{
+		fprintf(stderr, "Unable to create a new socket.");
+		exit(errno);
+	}
+
+	// 0 out
+	memset((char *) &sock, 0, sizeof(sock));
+
+	// Specify socket parameters
+	sock.sin_family = AF_INET;
+	sock.sin_port = htons(port);
+	sock.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// bind to specified socket
+	if (bind(socketfd, (struct sockaddr*) &sock, sizeof(sock)) == -1)
+	{
+		fprintf(stderr, "Error binding socket on port %d\n", port);
+		exit(errno);
+	}
+
+	return socketfd;
+}
+
+void signal_handler(int signal)
+{
+    printf("Terminating...");
+    keep_running = 0;
+}
+
+/**
+ * Checks if an ip address is in the same subnet as the defined prefix.
+ *
+ * Returns 0 if same, -1 if not.
+ */
+int compare_subnet(int prefix_length, char* candidate, char* destination)
+{
+	// build our mask
+	uint32_t net_mask = UINT_MAX;
+	uint32_t shift_amnt = 32 - prefix_length;
+
+	// shift right to unset lower bits, then shift back to get correct value
+	net_mask = net_mask >> shift_amnt;
+	net_mask = net_mask << shift_amnt;
+
+	uint32_t destination_ip = parse_ipv4_string(destination);
+
+	// gets the candidates subnet using the candidate with the mask
+	puts(destination);
+	puts(candidate);
+	uint32_t candidate_ip = parse_ipv4_string(candidate);
+	uint32_t subnet = candidate_ip & net_mask;
+	puts("candidate\n");
+	printf("%u\n", candidate_ip);
+
+	uint32_t masked_destination = destination_ip & net_mask;
+	puts("masked dest\n");
+	printf("%u\n", masked_destination);
+	puts("subnet\n");
+	printf("%u\n", subnet);
+	if (masked_destination == subnet)
+	{
+		puts("woo");
+		return 0;
+	}
+	
+	puts("fuck");
+	return -1;
+}
+
+/**
+ * Adopted from:
+ * http://stackoverflow.com/questions/10283703/conversion-of-ip-address-to-integer
+ *
+ * Which is a helper to convert IPv4 strings into unsigned ints for mask
+ * comparison.
+ */
+uint32_t parse_ipv4_string(char* ipAddress)
+{
+	uint32_t ipbytes[4];
+	sscanf(ipAddress, "%u.%u.%u.%u", &ipbytes[3], &ipbytes[2], &ipbytes[1], &ipbytes[0]);
+	return ipbytes[0] | ipbytes[1] << 8 | ipbytes[2] << 16 | ipbytes[3] << 24;
 }
--- assignment2/test.c
+++ assignment2/test.c
@@ -2,34 +2,45 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
-#include "router.h"
 
 /* Test Declarations */
-void test_build_route_table();
+void test_build_router_table();
 void test_receive_packet();
+void test_find_destination_router();
 
+RouterTable* table;
 
 /**
 * Test runner.
  */
 int main(int argc, char **argv)
 {
-    test_build_route_table();
+    test_build_router_table();
     test_receive_packet();
+    test_find_destination_router();
+
+    // now tear everything back down
+	for(int i = 0; i < table->size; i++)
+	{
+		free(table->routes[i]);
+	}
+	free(table);
 }
 
-void test_build_route_table()
+void test_build_router_table()
 {
     // basic parsing test
     char path[] = "./RT_A.txt";
-    RouteTable* table = build_route_table(path);
+    table = build_router_table(path);
     assert(table->size == 3);
 
     // make sure a route looks good
-    Route* route = table->routes[1];
-    assert(strcmp(route->address, "192.168.128.0") == 0);
-    assert(strcmp(route->next_hop, "0") == 0);
-    assert(route->prefix_length == 17);
+    Router* router = table->routes[1];
+    assert(strcmp(router->address, "192.168.128.0") == 0);
+    assert(strcmp(router->next_hop, "0") == 0);
+    assert(router->prefix_length == 17);
+
+    free(router);
 }
 
 void test_receive_packet()
@@ -48,4 +59,19 @@ void test_receive_packet()
     char raw_packet2[] = "215, 192.168.192.4, 192.224.0.7, 1, \"Hello\"";
     Packet* packet2 = build_packet(raw_packet2);
     assert(packet2 == NULL);
+
+    free(packet);
+    free(packet2);
+}
+
+void test_find_destination_router()
+{
+    Packet packet = (Packet) {125, "192.168.128.0", "192.168.192.0", 2, "\"yoooo\""};
+    Router* router = find_destination_router(table, &packet);
+    assert(router != NULL);
+    assert(strcmp(router->next_hop, "RouterB") == 0);
+
+    Packet packet2 = (Packet) {6290, "192.168.128.0", "192.168.567.0", 2, "\"Sweeet\""};
+    Router* router2 = find_destination_router(table, &packet2);
+    assert(router2 == NULL);
 }
